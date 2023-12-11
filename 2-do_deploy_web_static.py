f91e8a5dadae11a66c2674a4feaf546f2431e3cf
astronolt <odinaka.dev@gmail.com>
2023-12-11 09:25:09 +0800

    update web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["54.160.85.72", "35.175.132.106"]
+env.hosts = ["18.235.248.238", "52.90.13.126"]
 
 
 def do_deploy(archive_path):
